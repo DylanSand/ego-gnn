9d6d81ed1bed332d70010fc2912a2b73bc4a48ba
Dylan Sandfelder <dylansandfelder@gmail.com>
2020-11-25 01:11:20 -0500

    fix up some paths

--- ego_job_main.sh
+++ ego_job_main.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH --partition=unkillable                      # Ask for unkillable job
+#SBATCH --partition=unkillable                # Ask for unkillable job
 #SBATCH --cpus-per-task=2                     # Ask for 2 CPUs
 #SBATCH --gres=gpu:1                          # Ask for 1 GPU
 #SBATCH --mem=40G                             # Ask for 40 GB of RAM
-#SBATCH --time=30:00                        # The job will run for 1 hour
-#SBATCH -o /home/mila/d/dylan.sandfelder/slurm-%j/log.out  # Write the log on tmp1
+#SBATCH --time=30:00                          # The job will run for 0.5 hours
+#SBATCH -o ./slurm-%j/log.out	              # Write the log on tmp1
 # ----------------------
 
 rm ~/ego-gnn/model?.p
@@ -35,32 +35,6 @@ pip3 install -U scikit-learn
 #pip3 install plotly
 
 # ----------------------
-
-
-# 1. Load modules
-#module load pytorch
-#source $CONDA_ACTIVATE
-#module load cuda/10
-
-#python -c "import torch; print(torch.__version__)"
-#python -c "import torch; print(torch.cuda.is_available())"
-
-#python -c "import torch; print(torch.version.cuda)"
-#nvcc --version
-
-#export PATH=/usr/local/cuda/bin:$PATH
-#export CPATH=/usr/local/cuda/include:$CPATH
-#export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
-
-# 2. Install libraries
-#pip install --no-cache-dir torch-scatter==latest+cu101 torch-sparse==latest+cu101 torch-spline-conv==latest+cu101 torch-cluster==latest+cu101 -f https://pytorch-geometric.com/whl/torch-1.4.0.html
-#pip install --no-cache-dir torch-geometric
-#pip install torch==1.4.0+cu100 torchvision==0.5.0+cu100 -f https://download.pytorch.org/whl/torch_stable.html
-#pip install torch-scatter==2.0.2 -f https://pytorch-geometric.com/whl/torch-1.4.0.html
-#pip install torch-sparse==0.4.4 -f https://pytorch-geometric.com/whl/torch-1.4.0.html
-#pip install torch-cluster==1.4.5 -f https://pytorch-geometric.com/whl/torch-1.4.0.html
-#pip install torch-spline-conv==1.1.1 -f https://pytorch-geometric.com/whl/torch-1.4.0.html
-#pip install torch-geometric
 
 echo "Now copying data..."
 
